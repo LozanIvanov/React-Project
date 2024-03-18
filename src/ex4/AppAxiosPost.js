2ceca6a6846d33d464169ced1209c38ead0e426a
LozanIvanov <Lozan.Ivanov@abv.bg>
2024-03-18 20:38:40 +0200

    AxiosPost

---
+++ src/ex4/AppAxiosPost.js
@@ -0,0 +1,31 @@
+import React from "react";
+import axios from 'axios';
+import { useState } from "react";
+
+export default function AppPost(){
+    const[title,setTitle]=useState('');
+
+      function saveTodo(){
+        const todo={
+            completed:false,
+            title:title
+        }
+        axios.post('https://jsonplaceholder.typicode.com/todos',todo)
+        .then(response=>{
+            setTitle('');
+            alert('Success')
+        })
+      }
+    return(
+        <div>
+            <div>
+                <label>Title</label>
+                <imput type='text'value={title}
+                oninput={e=>setTitle(e.target.value)}/>
+            </div>
+            <div>
+                <button onClick={{savelTodo}} >Save</button>
+            </div>
+        </div>
+    )
+}
